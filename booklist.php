f0cfbf615cb55d7fd2d70caf91140cd635403b0a
msolorio <msolorio789@gmail.com>
2015-12-09 19:30:24 -0800

    initial adding of files

---
+++ booklist.php
@@ -0,0 +1,146 @@
+<!--
+based on Lincoln Edu wordpress theme on themeforest
+http://demo.lunartheme.com/lincoln/
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/normalize.css" type="text/css">
+	<link rel="stylesheet" href="css/main.css" type="text/css"/>
+	<link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900' rel='stylesheet' type='text/css'>
+	<script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+</head>
+<body>
+	<header>
+		<div class="login-bar bg-blue">
+			<div class="section-wrap">
+				<div class="login-text-div"><a href="" class="login-text-a text-white">Login</a> <span>&nbsp;/&nbsp;</span> <a href="" class="login-text-a text-white">Register</a></div>
+			</div>
+		</div>
+		<nav class="nav-bar bg-light-grey box-shadow">
+			<div class="section-wrap">
+				<div class="logo-container">
+					<img class="logo-img" src="img/library-logo.png" width="286" height="77" alt="">
+				</div>
+				<ul class="nav-ul">
+					<li class="nav-li"><a href="" class="nav-li-a">item 1</a>
+						<ul class="sub-menu-ul bg-light-grey box-shadow">
+							<li class="sub-menu-li"><a href="" class="sub-menu-li-a">sub menu 1</a></li>
+							<li class="sub-menu-li"><a href="" class="sub-menu-li-a">sub menu 2</a></li>
+							<li class="sub-menu-li"><a href="" class="sub-menu-li-a">sub menu 3</a></li>
+							<li class="sub-menu-li"><a href="" class="sub-menu-li-a">sub menu 4</a></li>
+							<li class="sub-menu-li"><a href="" class="sub-menu-li-a">sub menu 5</a></li>
+						</ul>
+					</li>
+					<li class="nav-li"><a href="" class="nav-li-a">item 2</a>
+						<ul class="sub-menu-ul bg-light-grey box-shadow">
+							<li class="sub-menu-li"><a href="" class="sub-menu-li-a">sub menu 1</a></li>
+							<li class="sub-menu-li"><a href="" class="sub-menu-li-a">sub menu 2</a></li>
+							<li class="sub-menu-li"><a href="" class="sub-menu-li-a">sub menu 3</a></li>
+							<li class="sub-menu-li"><a href="" class="sub-menu-li-a">sub menu 4</a></li>
+							<li class="sub-menu-li"><a href="" class="sub-menu-li-a">sub menu 5</a></li>
+						</ul>
+					</li>
+					<li class="nav-li"><a href="" class="nav-li-a">item 3</a>
+						<ul class="sub-menu-ul bg-light-grey box-shadow">
+							<li class="sub-menu-li"><a href="" class="sub-menu-li-a">sub menu 1</a></li>
+							<li class="sub-menu-li"><a href="" class="sub-menu-li-a">sub menu 2</a></li>
+							<li class="sub-menu-li"><a href="" class="sub-menu-li-a">sub menu 3</a></li>
+							<li class="sub-menu-li"><a href="" class="sub-menu-li-a">sub menu 4</a></li>
+							<li class="sub-menu-li"><a href="" class="sub-menu-li-a">sub menu 5</a></li>
+						</ul>
+					</li>
+					<li class="nav-li"><a href="" class="nav-li-a">item 4</a>
+						<ul class="sub-menu-ul bg-light-grey box-shadow">
+							<li class="sub-menu-li"><a href="" class="sub-menu-li-a">sub menu 1</a></li>
+							<li class="sub-menu-li"><a href="" class="sub-menu-li-a">sub menu 2</a></li>
+							<li class="sub-menu-li"><a href="" class="sub-menu-li-a">sub menu 3</a></li>
+							<li class="sub-menu-li"><a href="" class="sub-menu-li-a">sub menu 4</a></li>
+							<li class="sub-menu-li"><a href="" class="sub-menu-li-a">sub menu 5</a></li>
+						</ul>
+					</li>
+					<li class="nav-li"><a href="" class="nav-li-a">item 5</a>
+						<ul class="sub-menu-ul bg-light-grey box-shadow">
+							<li class="sub-menu-li"><a href="" class="sub-menu-li-a">sub menu 1</a></li>
+							<li class="sub-menu-li"><a href="" class="sub-menu-li-a">sub menu 2</a></li>
+							<li class="sub-menu-li"><a href="" class="sub-menu-li-a">sub menu 3</a></li>
+							<li class="sub-menu-li"><a href="" class="sub-menu-li-a">sub menu 4</a></li>
+							<li class="sub-menu-li"><a href="" class="sub-menu-li-a">sub menu 5</a></li>
+						</ul>
+					</li>
+				</ul>
+				<button class="hamburger">&#9776;</button>
+				<button class="cross">&#735;</button>
+			</div>
+		</nav>
+	</header>
+	<nav class="hamburger-nav">
+		<ul class="hamburger-nav-ul bg-dark-grey2">
+			<li class="hamburger-nav-li"><a href="" class="hamburger-nav-a text-medium-grey font-light">item 1</a></li>
+			<li class="hamburger-nav-li"><a href="" class="hamburger-nav-a text-medium-grey font-light">item 2</a></li>
+			<li class="hamburger-nav-li"><a href="" class="hamburger-nav-a text-medium-grey font-light">item 3</a></li>
+			<li class="hamburger-nav-li hamburger-nav-li-last"><a href="" class="hamburger-nav-a text-medium-grey font-light">item 4</a></li>
+		</ul>
+	</nav>
+	<main>
+		<div class="section-wrap">
+		</div>
+	</main>
+	<footer class="bg-dark-grey font-light">
+		<div class="footer-menu">
+			<ul class="footer-menu-ul">
+				<li class="footer-menu-li"><a href="" class="footer-menu-a text-medium-grey font-light">item 1</a>
+					<ul class="footer-sub-menu-ul">
+						<li class="footer-sub-menu-li"><a href="" class="footer-sub-menu-a font-light text-medium-grey">sub menu 1</a></li>
+						<li class="footer-sub-menu-li"><a href="" class="footer-sub-menu-a font-light text-medium-grey">sub menu 2</a></li>
+						<li class="footer-sub-menu-li"><a href="" class="footer-sub-menu-a font-light text-medium-grey">sub menu 3</a></li>
+						<li class="footer-sub-menu-li"><a href="" class="footer-sub-menu-a font-light text-medium-grey">sub menu 4</a></li>
+						<li class="footer-sub-menu-li"><a href="" class="footer-sub-menu-a font-light text-medium-grey">sub menu 5</a></li>
+					</ul>
+				</li>
+				<li class="footer-menu-li"><a href="" class="footer-menu-a text-medium-grey font-light">item 2</a>
+					<ul class="footer-sub-menu-ul">
+						<li class="footer-sub-menu-li"><a href="" class="footer-sub-menu-a font-light text-medium-grey">sub menu 1</a></li>
+						<li class="footer-sub-menu-li"><a href="" class="footer-sub-menu-a font-light text-medium-grey">sub menu 2</a></li>
+						<li class="footer-sub-menu-li"><a href="" class="footer-sub-menu-a font-light text-medium-grey">sub menu 3</a></li>
+						<li class="footer-sub-menu-li"><a href="" class="footer-sub-menu-a font-light text-medium-grey">sub menu 4</a></li>
+						<li class="footer-sub-menu-li"><a href="" class="footer-sub-menu-a font-light text-medium-grey">sub menu 5</a></li>
+					</ul>
+				</li>
+				<li class="footer-menu-li"><a href="" class="footer-menu-a text-medium-grey font-light">item 3</a>
+					<ul class="footer-sub-menu-ul">
+						<li class="footer-sub-menu-li"><a href="" class="footer-sub-menu-a font-light text-medium-grey">sub menu 1</a></li>
+						<li class="footer-sub-menu-li"><a href="" class="footer-sub-menu-a font-light text-medium-grey">sub menu 2</a></li>
+						<li class="footer-sub-menu-li"><a href="" class="footer-sub-menu-a font-light text-medium-grey">sub menu 3</a></li>
+						<li class="footer-sub-menu-li"><a href="" class="footer-sub-menu-a font-light text-medium-grey">sub menu 4</a></li>
+						<li class="footer-sub-menu-li"><a href="" class="footer-sub-menu-a font-light text-medium-grey">sub menu 5</a></li>
+					</ul>
+				</li>
+				<li class="footer-menu-li"><a href="" class="footer-menu-a text-medium-grey font-light">item 4</a>
+					<ul class="footer-sub-menu-ul">
+						<li class="footer-sub-menu-li"><a href="" class="footer-sub-menu-a font-light text-medium-grey">sub menu 1</a></li>
+						<li class="footer-sub-menu-li"><a href="" class="footer-sub-menu-a font-light text-medium-grey">sub menu 2</a></li>
+						<li class="footer-sub-menu-li"><a href="" class="footer-sub-menu-a font-light text-medium-grey">sub menu 3</a></li>
+						<li class="footer-sub-menu-li"><a href="" class="footer-sub-menu-a font-light text-medium-grey">sub menu 4</a></li>
+						<li class="footer-sub-menu-li"><a href="" class="footer-sub-menu-a font-light text-medium-grey">sub menu 5</a></li>
+					</ul>
+				</li>
+				<li class="footer-menu-li"><a href="" class="footer-menu-a text-medium-grey font-light">item 5</a>
+					<ul class="footer-sub-menu-ul">
+						<li class="footer-sub-menu-li"><a href="" class="footer-sub-menu-a font-light text-medium-grey">sub menu 1</a></li>
+						<li class="footer-sub-menu-li"><a href="" class="footer-sub-menu-a font-light text-medium-grey">sub menu 2</a></li>
+						<li class="footer-sub-menu-li"><a href="" class="footer-sub-menu-a font-light text-medium-grey">sub menu 3</a></li>
+						<li class="footer-sub-menu-li"><a href="" class="footer-sub-menu-a font-light text-medium-grey">sub menu 4</a></li>
+						<li class="footer-sub-menu-li"><a href="" class="footer-sub-menu-a font-light text-medium-grey">sub menu 5</a></li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+		<div class="copyright text-medium-grey">&copy;Copyright, All Rights Reserved</div>
+	</footer>
+</body>
+</html>
